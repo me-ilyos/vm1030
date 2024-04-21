b13db3afb39164dd15fa3b67a7b43cfe4373f49f
me.ilyos <me.ilyos101.com>
2024-04-21 19:32:43 +0500

    Setup flake and black

---
+++ vm1030/settings/local.py
@@ -0,0 +1,22 @@
+import os
+
+from .settings import *
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+DEBUG = True
+
+SECRET_KEY = os.environ["SECRET_KEY"]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ["DB_NAME"],
+        "USER": os.environ["DB_USER"],
+        "PASSWORD": os.environ["DB_PASSWORD"],
+        "HOST": os.environ["DB_HOST"],
+        "PORT": os.environ["PORT"],
+    }
+}
